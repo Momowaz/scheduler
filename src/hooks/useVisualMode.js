40178c464dcf33a2880da4d7ce237155ce29fab3
Momowaz <momo.waz1593@gmail.com>
2023-07-17 18:46:41 -0700

    test useVisualMode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+
+function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    setMode(newMode);
+    setHistory(prevHistory => {
+      const updatedHistory = replace ? [...prevHistory.slice(0, -1), newMode] : [...prevHistory, newMode];
+      return updatedHistory;
+    });
+  }
+
+  function back() {
+    if (history.length > 1) {
+      const previousMode = history[history.length - 2];
+      setMode(previousMode);
+      setHistory(prevHistory => prevHistory.slice(0, -1));
+    } else {
+      setMode(initial);
+      setHistory([initial]);
+    }
+  }
+
+
+  return {
+    mode,
+    transition,
+    back
+  };
+}
+
+export { useVisualMode };
